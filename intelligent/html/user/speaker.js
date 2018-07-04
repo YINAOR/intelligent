89425e7f4294c46c745bd0a102175e68b3ffada9
YyinWK <15626135769@163.com>
2018-07-04 15:10:15 +0800

    Signed-off-by: YyinWK <15626135769@163.com>

--- intelligent/html/user/speaker.js
+++ intelligent/html/user/speaker.js
@@ -3,4 +3,28 @@
     _g.setNowPage('user/speaker');
     $('#formContent').html(_g.getTemplate('user/speaker-V'));
 
+    var data = {
+        currentPage: 1,
+        pageSize: 5
+    }
+
+    function getList() {
+        $.ajax({
+            url: 'http://118.89.26.114/speakerAndLecType/queryAllSpeaker.do',
+            dataType: 'json',
+            type: 'POST',
+            async: false,
+            processData: false,
+            contentType: 'application/json',
+            data:JSON.stringify({
+                currentPage: 1,
+                pageSize: 5
+            }),
+            success: function(result) {
+                var data1 = { list: result };
+                _g.render('user/speaker-V', data1, '#table');
+            },
+        });
+    }
+    getList();
 })();
