43ccc12e2f8d58f44438a8affe21d05ec97ceb77
wenxiao123 <821953863@qq.com>
2018-07-25 17:35:52 +0800

    动态二维码调用

--- intelligent/html/lecture/sign.js
+++ intelligent/html/lecture/sign.js
@@ -5,26 +5,38 @@
 
     function getQrCode() {
         var supports = (new XMLHttpRequest()).withCredentials !== undefined;
-        alert(supports);
-        $.ajax({
-            url: 'http://lai.vipgzl.idcfengye.com/WebTest3/qRCodeServlet',
-            dataType: 'json',
-            type: 'POST',
-            async: false,
-            processData: false,
-            contentType: 'application/json',
-            data: JSON.stringify({
+        if (supports) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("POST", "http://lai.vipgz1.idcfengye.com/WebTest3/qRCodeServlet", true);
+            xmlhttp.setRequestHeader('Content-Type', 'application/json');
+            xmlhttp.responseType = "blob";
+            xmlhttp.onload = function() {
+                console.log(this);
+                if (this.status == 200) {
+                    if(set) {
+                        clearInterval(set);
+                    }
+                    var blob = this.response;  
+                    var img = document.createElement('img');  
+                    img.onload = function (e) {  
+                        window.URL.revokeObjectURL(img.src);  
+                    };  
+                    img.src = window.URL.createObjectURL(blob);  
+                    $('#item').html(img);
+                    $('#item img').addClass('img-full');
+                }
+            }
+            xmlhttp.send(JSON.stringify({
                 QRCode: 'Y',
                 status: 'S',
                 lecture: 1
-            }),
-            success: function(result) {
-                alert(111)
-            },
-        })
+            }));
+        }
+
     }
 
     getQrCode();
+    var set = setInterval(getQrCode, 6000);
 
 
 })();
