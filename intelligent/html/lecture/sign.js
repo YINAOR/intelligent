c8665f148629e8ae04331e3249d9ac654541944f
wenxiao123 <821953863@qq.com>
2018-07-26 00:02:34 +0800

    解决时间间隔错误

--- intelligent/html/lecture/sign.js
+++ intelligent/html/lecture/sign.js
@@ -3,19 +3,23 @@
     _g.setNowPage('lecture/sign');
     $('#formContent').html(_g.getTemplate('lecture/sign-V'));
 
-    function getQrCode() {
+    function getQrCode(getTime) {
         var supports = (new XMLHttpRequest()).withCredentials !== undefined;
         if (supports) {
             var xmlhttp = new XMLHttpRequest();
             xmlhttp.open("POST", "http://lai.vipgz1.idcfengye.com/WebTest3/qRCodeServlet", true);
             xmlhttp.setRequestHeader('Content-Type', 'application/json');
             xmlhttp.responseType = "blob";
+            if(getTime) {
+                xmlhttp.onreadystatechange = function() {
+                    if(xmlhttp.readyState != 4) {
+                        $('#item').html('<div class="ui-loading__bd"><img src="/images/loading_more.gif" alt=""></div>');
+                    }
+                }
+            }
             xmlhttp.onload = function() {
                 console.log(this);
                 if (this.status == 200) {
-                    if(set) {
-                        clearInterval(set);
-                    }
                     var blob = this.response;  
                     var img = document.createElement('img');  
                     img.onload = function (e) {  
@@ -35,8 +39,25 @@
 
     }
 
-    getQrCode();
-    var set = setInterval(getQrCode, 6000);
+    getQrCode('first');
+    
+    var set = setTimeout(function () {
+        getQrCode();
+        setTimeout(arguments.callee, 6000);
+    }, 6000);
 
+//     if( ('onhashchange' in window) && ((typeof document.documentMode==='undefined') || document.documentMode==8)) {
+//     // 浏览器支持onhashchange事件
+//     window.onhashchange = clearTimeout(set);  // TODO，对应新的hash执行的操作函数
+// } else {
+//     // 不支持则用定时器检测的办法
+//     setInterval(function() {
+//         // 检测hash值或其中某一段是否更改的函数， 在低版本的iE浏览器中通过window.location.hash取出的指和其它的浏览器不同，要注意
+// 　　　　 var ischanged = isHashChanged(); 
+//         if(ischanged) {
+//             clearTimeout(set);  // TODO，对应新的hash执行的操作函数
+//         }
+//     }, 150);
+// }
 
 })();
