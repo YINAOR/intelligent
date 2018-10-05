bda0665e4fddf17e014211a1de033843ad9c3588
YyinWK <15626135769@163.com>
2018-10-06 00:10:15 +0800

    Signed-off-by: YyinWK <15626135769@163.com>

--- intelligent/html/lecture/listType.js
+++ intelligent/html/lecture/listType.js
@@ -57,6 +57,41 @@
             }
         });
     }
+
     getList();
+
+    deleteItem = function(id) {
+        _g.ajax({
+            lock: true,
+            url: 'http://120.77.204.252:80/category/delete.do',
+            async: false,
+            data: {
+                id: id
+            },
+            success:function(result){
+                if(result.code === 200){
+                    layer.open({
+                        title: '消息',
+                        content: result.msg,
+                    });
+                    getList();
+                } else if(result.code === 1000){
+                    layer.open({
+                        title: '消息',
+                        content: result.msg,
+                        yes: function(index){
+                            layer.close(index);
+                            window.location.href = '/signin.html';
+                        }
+                    });
+                } else {
+                    layer.open({
+                        title: '消息',
+                        content: result.msg,
+                    })
+                }
+            }
+        })
+    }
 
 })();
