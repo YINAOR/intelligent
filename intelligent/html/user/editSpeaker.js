26fda1371cd8cbf5dc12d4196a4fa3b915f57280
wenxiao123 <821953863@qq.com>
2018-10-06 21:48:31 +0800

    更新讲师

--- intelligent/html/user/editSpeaker.js
+++ intelligent/html/user/editSpeaker.js
@@ -3,8 +3,9 @@
     _g.setNowPage('user/editSpeaker');
     $('#formContent').html(_g.getTemplate('user/editSpeaker-V'));
 
-
+    
     var id = _g.pm.param.id;
+    var firstFileName;
     if(id) {
         $('.head').text('编辑主讲人');
         $('.speakerId').show();
@@ -21,9 +22,10 @@
                     $('#spname').val(speaker.name);
                     $('#gender li input[value='+ speaker.gender +']').parent().addClass('active');
                     $('.dropdown-label').text(speaker.gender === 0 ? '男' : '女');
+                    firstFileName = speaker.imageUrl;
                     if(speaker.imageUrl != '') {
                         $('#file').val(speaker.imageUrl);
-                        $('#prePhoto').append('<img src="http://120.77.204.252:80/' + speaker.imageUrl +'" style="width：100px;height: 150px;"/>');
+                        $('#prePhoto').append('<img src="http://120.77.204.252:80' + speaker.imageUrl +'" style="width：100px;height: 150px;"/>');
                     }
                     $('#spbrief').val(speaker.brief);
                 } else if(result.code === 1000){
@@ -82,64 +84,72 @@
             var token = sessionStorage.getItem('token');
             var url = 'http://120.77.204.252:80/speaker/save.do?token='+ token +'&uploadsign=speaker';
             var formData = new FormData($('.form-horizontal')[0]);
+
             if(!id) {
                 formData.delete('id');
             }
             formData.delete('d-s-r');
             formData.append('gender', gender);
             if(id){
-                url = 'http://120.77.204.252:80/speaker/update.do?token='+ token +'&uploadsign=speaker';
-
-                function convertBase64UrlToBlob(base64){ 
-                    var urlData =  base64.dataURL;
-                    var type = base64.type;
-                    var bytes = window.atob(urlData.split(',')[1]); //去掉url的头，并转换为byte
-                    //处理异常,将ascii码小于0的转换为大于0  
-                    var ab = new ArrayBuffer(bytes.length);  
-                    var ia = new Uint8Array(ab);  
-                    for (var i = 0; i < bytes.length; i++) {  
-                        ia[i] = bytes.charCodeAt(i);  
-                    }  
-                    return new Blob( [ab] , {type : type});  
+                url = 'http://lai.vipgz1.idcfengye.com/intelligent/speaker/update.do?token='+ token +'&uploadsign=speaker';
+                formData.append('id', id);
+                if(firstFileName == $('#file').val()) {
+                    formData.delete('imagefile');
                 }
-      /* 
-       * 图片的绝对路径地址 转换成base64编码 如下代码： 
-       */
-                function getBase64Image(img) {
-                    var canvas = document.createElement("canvas");
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    var ctx = canvas.getContext("2d");
-                    ctx.drawImage(img, 0, 0, img.width, img.height);
-                    var ext = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();
-                    var dataURL = canvas.toDataURL("image/"+ext);
-                    return {
-                        dataURL: dataURL,
-                        type: "image/"+ext
-                    };
-                }
-                var img = 'http://120.77.204.252:80' + $('#file').val();
-                var image = new Image();
-                image.crossOrigin = '';
-                image.src = img;
-                image.onload = function(){
-                    var base64 = getBase64Image(image);
-                    console.log(base64);
-        /*
-         打印信息如下：
-         {
-          dataURL: "data:image/png;base64,xxx"
-          type: "image/jpg"
-         }
-         */
-                    var img2 = convertBase64UrlToBlob(base64);
-                    console.log(img2);
-        /*
-         打印信息如下：
-         Blob {size: 9585, type: "image/jpg"}
-         */     }
-                formData.set('imagefile', img2);
             } 
+      //           function convertBase64UrlToBlob(base64){ 
+      //               var urlData =  base64.dataURL;
+      //               var type = base64.type;
+      //               var bytes = window.atob(urlData.split(',')[1]); //去掉url的头，并转换为byte
+      //               //处理异常,将ascii码小于0的转换为大于0  
+      //               var ab = new ArrayBuffer(bytes.length);  
+      //               var ia = new Uint8Array(ab);  
+      //               for (var i = 0; i < bytes.length; i++) {  
+      //                   ia[i] = bytes.charCodeAt(i);  
+      //               }  
+      //               return new Blob( [ab] , {type : type});  
+      //           }
+      // /* 
+      //  * 图片的绝对路径地址 转换成base64编码 如下代码： 
+      //  */
+      //           function getBase64Image(img) {
+      //               var canvas = document.createElement("canvas");
+      //               canvas.width = img.width;
+      //               canvas.height = img.height;
+      //               var ctx = canvas.getContext("2d");
+      //               ctx.drawImage(img, 0, 0, img.width, img.height);
+      //               var ext = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();
+      //               var dataURL = canvas.toDataURL("image/"+ext);
+      //               return {
+      //                   dataURL: dataURL,
+      //                   type: "image/"+ext
+      //               };
+      //           }
+      //           var img = 'http://120.77.204.252:80' + $('#file').val();
+      //           var image = new Image();
+      //           image.crossOrigin = '';
+      //           image.src = img;
+      //           image.onload = function(){
+      //               var base64 = getBase64Image(image);
+      //               console.log(base64);
+      //   /*
+      //    打印信息如下：
+      //    {
+      //     dataURL: "data:image/png;base64,xxx"
+      //     type: "image/jpg"
+      //    }
+      //    */
+      //               var img2 = convertBase64UrlToBlob(base64);
+      //               console.log(img2);
+      //               formData.set('imagefile', img2);
+      //   /*
+      //    打印信息如下：
+      //    Blob {size: 9585, type: "image/jpg"}
+      //    */     }
+                
+            
+            document.activeElement.blur();
+            $('.ui-loading').show();
             $.ajax({
                 url: url,
                 dataType:"json",
@@ -149,6 +159,7 @@
                 processData:false,
                 data: formData,
                 success: function(result) {
+                    $('.ui-loading').hide();
                     if(result.code === 200) {
                         layer.open({
                             title: '消息',
@@ -172,6 +183,7 @@
                     }
                 },
                 error: function(error) {
+                    $('.ui-loading').hide();
                     layer.open({
                        title: '消息',
                        content: '请求超时，请重试！',
